15802cf496400a7e45e2861bae2c3afcbd860503
NSG650 <nsg650@waifu.club>
2023-01-20 12:22:22 +0530

    finally it works on polaris

--- ports/minimal/mpconfigport.h
+++ ports/minimal/mpconfigport.h
@@ -28,12 +28,12 @@ typedef long mp_off_t;
 // We need to provide a declaration/definition of alloca()
 #include <alloca.h>
 
-#define MICROPY_HW_BOARD_NAME "minimal"
-#define MICROPY_HW_MCU_NAME "unknown-cpu"
-
-#ifdef __linux__
-#define MICROPY_MIN_USE_STDOUT (1)
+#define MICROPY_HW_BOARD_NAME "polaris"
+#if defined(__x86_64__)
+#define MICROPY_HW_MCU_NAME "x86_64-cpu"
 #endif
+
+#define MICROPY_MIN_USE_STDOUT (1)
 
 #ifdef __thumb__
 #define MICROPY_MIN_USE_CORTEX_CPU (1)
